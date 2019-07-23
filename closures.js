d4c9e8651ac7d04d84cda50966bfbbfaf0ff63bd
Max-F <max.fullmer@outlook.com>
2019-07-23 16:21:20 -0700

    July 23, 2019

--- closures.js
+++ closures.js
@@ -24,13 +24,13 @@ function outer() {
   
 // Code Here
 
-
+let inner = outer();
 
 //Once you do that, invoke inner.
 
 //Code Here
 
-
+inner();
 
 ////////// PROBLEM 2 //////////
 
@@ -53,7 +53,8 @@ function callFriend(name) {
 
 //Code Here
 
-
+let callJake = callFriend('Jake');
+callJake('435-555-9248');
 
 ////////// PROBLEM 3 //////////
 
@@ -63,14 +64,20 @@ function callFriend(name) {
 
 //Code Here
 
-
+function makeCounter() {
+  counterr = 0;
+  function count() {
+    return counterr += 1;
+  }
+  return count;
+}
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -89,15 +96,23 @@ function counterFactory(value) {
   // Code here.
 
   return {
-
+    inc: function() {
+      value += 1;
+      return value;
+    },
+    
+    dec: function() {
+      value -= 1;
+      return value;
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +128,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+    function message() {
+      return `${welcomeText} ${firstname} ${lastname}.`
+    }
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,6 +161,9 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function() {
+      return privateMethod();
+    }
   };
 })();
 
@@ -163,6 +183,12 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(num) {
+      return secret += num
+    },
+    takeAwayFromSecret: function(num) {
+      return secret -= num
+    }
   };
 }
 
@@ -187,10 +213,11 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
+  for (let i = 0; i <= 5; i++) {
+      setTimeout(function() {
       console.log(i);
-    }, i * 1000);
+      }, i * 1000);
   }
 }
+
 timeOutCounter();
